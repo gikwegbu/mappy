7e980f154d581080c9af26f1bfe4d5185eae37aa
George  Ikwegbu Chinedu <39649680+gikwegbu@users.noreply.github.com>
2023-03-30 05:08:58 +0100

    Initial commit for the IOS mAp application

---
+++ mappy/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  mappy
+//
+//  Created by gikwegbu on 30/03/2023.
+//
+
+import Foundation
+import MapKit
+
+
+struct Location {
+	let name: String
+	let cityName: String
+	let coordinates: CLLocationCoordinate2D
+	let description: String
+	let imageNames: [String]
+	let link: String
+}
+
---
+++ mappy/mappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mappyApp.swift
+//  mappy
+//
+//  Created by gikwegbu on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct mappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
